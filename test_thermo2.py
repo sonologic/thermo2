0a44c1795db58630e3d2a363421f124ba67ea248
Koen Martens <gmc@sonologic.nl>
2014-01-04 01:49:02 +0100

    Use setUp method.

--- test_thermo2.py
+++ test_thermo2.py
@@ -3,12 +3,11 @@ from thermo2 import Thermo2
 
 class Thermo2Test(unittest.TestCase):
 
-    def test_thermo2_initialization(self):
-        thermo2 = Thermo2()
+    def setUp(self):
+        self.thermo2 = Thermo2()
 
     def test_thermo2_run(self):
-        thermo2 = Thermo2()
-        self.assertEqual(thermo2.run(), 0)
+        self.assertEqual(self.thermo2.run(), 0)
 
 if __name__ == "__main__":
     unittest.main()

2f3c4e95d3e9886936b9658476cc5f74f19d675c
Koen Martens <gmc@sonologic.nl>
2014-01-04 01:47:19 +0100

    Adjust test-case to call run method.

--- test_thermo2.py
+++ test_thermo2.py
@@ -6,9 +6,9 @@ class Thermo2Test(unittest.TestCase):
     def test_thermo2_initialization(self):
         thermo2 = Thermo2()
 
-    def test_thermo2_iterate(self):
+    def test_thermo2_run(self):
         thermo2 = Thermo2()
-        self.assertIsInstance(thermo2.iterate(), int)
+        self.assertEqual(thermo2.run(), 0)
 
 if __name__ == "__main__":
     unittest.main()

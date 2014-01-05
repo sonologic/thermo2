1933f46aef4996c2fd43e7db4e3092fa01d3bf77
Koen Martens <gmc@sonologic.nl>
2014-01-05 17:48:00 +0100

    Refactor test, join suites and run instead of run seperately.

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import unittest
 
 from test_thermo2 import Thermo2Test
 from test_scheduler import SchedulerSetupTest
+from test_timer import TimerTest
 
 suite1 = unittest.TestLoader().loadTestsFromTestCase(Thermo2Test)
 suite2 = unittest.TestLoader().loadTestsFromTestCase(SchedulerSetupTest)
+suite3 = unittest.TestLoader().loadTestsFromTestCase(TimerTest)
 
-unittest.TextTestRunner(verbosity=2).run(suite1)
-unittest.TextTestRunner(verbosity=2).run(suite2)
+suite = unittest.TestSuite([suite1, suite2, suite3])
+
+unittest.TextTestRunner(verbosity=2).run(suite)
 

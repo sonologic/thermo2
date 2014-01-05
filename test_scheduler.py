427604aed6b2eae9748d13ef0728ed23c061797b
Koen Martens <gmc@sonologic.nl>
2014-01-05 17:49:11 +0100

    Add testcases for timer logic in scheduler.

--- test_scheduler.py
+++ test_scheduler.py
@@ -1,13 +1,42 @@
 import unittest
 from scheduler import Scheduler
+from time import time
 
 class SchedulerSetupTest(unittest.TestCase):
 
-    def setUp(self):
+    def test_scheduler_schedule(self):
+        self.scheduler = Scheduler()
+        self.assertEqual(self.scheduler.schedule(),'idle')
+
+    def test_scheduler_timer(self):
+        self.scheduler = Scheduler()
+        error = 0.1
+        delay = 2
+
+        startTime = time()
+        self.scheduler.add_timer(delay,'test_timer')
+        while time() < startTime+delay-error:
+            self.assertEqual(self.scheduler.schedule(),'idle')
+
+        event_count = 0
+
+        while time() < startTime+delay+error:
+            if(self.scheduler.schedule()=='test_timer'):
+                event_count = event_count + 1
+        
+
+        self.assertEqual(event_count,1)
+
+    def test_scheduler_add_timer(self):
         self.scheduler = Scheduler()
 
-    def test_scheduler_schedule(self):
-        self.assertEqual(self.scheduler.schedule(),'idle')
+        self.scheduler.add_timer(2, 'test_timer')
+
+        self.assertEqual(len(self.scheduler.timers),1);
+
+        self.assertTrue('test_timer' in self.scheduler.timers.keys())
+
+        self.assertEqual(self.scheduler.timers['test_timer'].getPeriod(),2)
 
 if __name__ == "__main__":
     unittest.main()

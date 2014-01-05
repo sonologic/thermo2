7f9ae7c4572413d0f3ebbb604584feec6604f0de
Koen Martens <gmc@sonologic.nl>
2014-01-05 17:49:31 +0100

    Implement scheduler timer logic to satisfy test-cases.

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,17 @@
+from timer import Timer
+
 class Scheduler:
     def __init__(self):
-        pass
+        self.timers={}
 
     def schedule(self):
+        for label in self.timers.keys():
+            trigger = self.timers[label].poll()
+            if not trigger == None:
+                return label 
         return 'idle'
+
+    def add_timer(self,period,label):
+        timer = Timer(period)
+        self.timers[label]=timer
+

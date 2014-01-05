d5feb651e85067c5ad60e1288e537622b1b9344c
Koen Martens <gmc@sonologic.nl>
2014-01-05 18:07:50 +0100

    Add EventTest to test suite.

--- test.py
+++ test.py
@@ -3,12 +3,14 @@ import unittest
 from test_thermo2 import Thermo2Test
 from test_scheduler import SchedulerTest
 from test_timer import TimerTest
+from test_event import EventTest
 
-suite1 = unittest.TestLoader().loadTestsFromTestCase(Thermo2Test)
-suite2 = unittest.TestLoader().loadTestsFromTestCase(SchedulerTest)
-suite3 = unittest.TestLoader().loadTestsFromTestCase(TimerTest)
+suite = []
 
-suite = unittest.TestSuite([suite1, suite2, suite3])
+suite.append(unittest.TestLoader().loadTestsFromTestCase(Thermo2Test))
+suite.append(unittest.TestLoader().loadTestsFromTestCase(SchedulerTest))
+suite.append(unittest.TestLoader().loadTestsFromTestCase(TimerTest))
+suite.append(unittest.TestLoader().loadTestsFromTestCase(EventTest))
 
-unittest.TextTestRunner(verbosity=2).run(suite)
+unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(suite))
 

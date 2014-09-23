549926877ab96bfccdd2d47b62da4640dda21bdb
Koen Martens <gmc@sonologic.nl>
2014-09-23 21:09:50 +0200

    Add documentation to EventListener.

--- event_listener.py
+++ event_listener.py
@@ -1,13 +1,35 @@
 class EventListener:
+    """An EventListener can be registered with the scheduler, keyed on
+    event label.
+
+    When the scheduler processes an event for which this listener is registered
+    (one listener may be registered on various events), the event method is
+    invoked.
+    """
 
     def __init__(self):
         self.events = []
 
     def addEvent(self,event):
+        """Add an event label to the sensitivity list.
+
+        Changes to the sensitivity list after the listener has been registered
+        with the scheduler will be ignored.
+        """
         self.events.append(event)
 
     def eventList(self):
         return self.events
 
     def event(self,event):
+        """Callback for events.
+
+        Called by the scheduler. This listener may be registered to multiple
+        events, in that case the overriding method must distinguish based on 
+        the event object's label.
+
+        Returns a list of events (possibly empty). If the event that was passed
+        to this method is in the return list, the scheduler will continue to try
+        other listeners.
+        """ 
         return [event]

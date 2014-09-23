4da98a3d667448cb3ad60676ce14a6292060105a
Koen Martens <gmc@sonologic.nl>
2014-09-23 20:50:42 +0200

    Add documentation.

---
+++ README.txt
@@ -0,0 +1,21 @@
+Use cases
+=========
+
+1wire temp sensor
+-----------------
+
+1 start sensor read
+2 read sensor
+3 event (if changed)
+
+arp sensor
+----------
+
+1 fetch munin value
+2 event (if changed)
+
+knob
+----
+
+1 fetch knob value
+2 event (if changed)
--- scheduler.py
+++ scheduler.py
@@ -9,40 +9,64 @@ class Scheduler:
         self.events=[]
 
     def schedule(self, events=None):
-        if not events == None:
-            #print "in "+str(events)
-            for event in events:
-                #print "  append "+event.getLabel()
-                self.events.append(event)
-            #print "events queued "+str(self.events)
-        #print "listeners "+str(self.listeners)
+        """Invoke scheduler.
 
+        Optionally accepts a list of events to inject into the event queue.
+
+        Returns the label of the processed event, or 'idle'. A maximum of one
+        event is processed per invocation of schedule().
+
+        The scheduler:
+        1. injects the optional list of events
+        2. injects event on timer expiries
+        3. processes the event queue
+            - consume oldest element on queue, call listeners
+            - returns the label of the event
+        4. return 'idle'
+        
+        """
+   
+        # process optional events by adding to event queue 
+        if not events == None:
+            for event in events:
+                self.events.append(event)
+
+        # check all timers, inject events on expiry
         for label in self.timers.keys():
             trigger = self.timers[label].poll()
             if not trigger == None:
                 self.events.append(Event(time(), label))
 
+        # process event queue:
         for event in self.events:
-            #print "--> "+str(event.getTime())+": "+event.getLabel()
-            #print "--> l "+str(self.listeners)
+            # check if anyone cares about this event:
             if event.getLabel() in self.listeners.keys():
+                # if so, loop over list of interested listeners
                 for listener in self.listeners[event.getLabel()]:
-                    #print "listener "+str(listener)
+
+                    # invoke listener:
                     newEvents = listener.event(event)
 
+                    # process events returned by listener
                     retry = False
-
                     for newEvent in newEvents:
                         if newEvent == event:
                             retry = True
                         else:
                             self.events.append(newEvent)
 
+                    # if listener did not return the original event, listener has consumed it
+                    # do not attempt further event listeners
                     if not retry:
                         break
+        
+            # consume event
             self.events = self.events[1:]
+            
+            # and return the label of the processed event
             return event.getLabel()
-                    
+                   
+        # if no events were to be processed, return idle state 
         return 'idle'
 
     def add_timer(self,period,label):
@@ -51,8 +75,6 @@ class Scheduler:
 
     def add_listener(self,listener):
         for event in listener.eventList():
-            #print "addloop "+event
             if not event in self.listeners.keys():
                 self.listeners[event] = []
         self.listeners[event].append(listener)
-        #print "add result "+str(self.listeners)

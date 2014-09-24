7aef570b44606d155b2854da46ce9833ff86ab96
Koen Martens <gmc@sonologic.nl>
2014-09-24 20:31:35 +0200

    Add test-case for Script evaluation.

--- test/test_script.py
+++ test/test_script.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == "__main__":
     sys.path.insert(0, "../")
 
+from sensor_event import *
 from script import *
 
 class ScriptTest(unittest.TestCase):
@@ -12,6 +13,7 @@ class ScriptTest(unittest.TestCase):
     def test_parser(self):
         script = Script('''sum := op1_value + op2_value
                            emit sum_value(sum)''')
+
     def test_parser_nl_ending(self):
         script = Script('''sum := op1_value + op2_value
                            emit sum_value(sum)
@@ -24,6 +26,27 @@ class ScriptTest(unittest.TestCase):
             pass
         else:
             self.fail("Expected a ScriptParseError")
+
+    def test_eval(self):
+        script = Script('''sum := op1_value + op2_value
+                           emit sum_value(sum)''')
+
+        e1 = SensorEvent(time(), 'op1_value', 2)
+        rv = script.eval(e1)
+
+        self.assertEqual(rv,[])
+
+        e2 = SensorEvent(time(), 'op2_value', 3)
+        rv = script.eval(e2)
+
+        self.assertEqual(isinstance(rv,list),True)
+        self.assertEqual(len(rv),1)
+
+        e3 = rv[0]
+
+        self.assertEqual(isinstance(e3,SensorEvent),True)
+        self.assertEqual(e3.getLabel(),'sum_value')
+        self.assertEqual(e3.getValue(),5)
 
 
 if __name__ == "__main__":

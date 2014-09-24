030a1e031f4bae5a5c55ae9ee767e860d7fff5ab
Koen Martens <gmc@sonologic.nl>
2014-09-24 20:36:13 +0200

    Refactor Script parser to use parser_constants.

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import re
+from parser_constants import ParserConstants
 
 class ScriptParseError(Exception):
     def __init__(self,str):
@@ -13,16 +14,15 @@ class Script:
         lineno = 1
         for line in str.split("\n"):
             # assignment
-            if re.match('\s*([A-Za-z0-9]+)\s*:=\s*(.*)\s*$', line):
+            if re.match('\s*('+ParserConstants.RE_IDENTIFIER+')\s*:=\s*(.*)\s*$', line):
                 return
             
             # emit 
-            if re.match('^\s*emit\s*([A-Za-z0-9]+)\s*([A-Za-z0-9]+)\s*\(\s*([A-Za-z0-9]+)\s*\)\s*$', line):
+            if re.match('^\s*emit\s*('+ParserConstants.RE_IDENTIFIER+')\s*\(\s*('+ParserConstants.RE_IDENTIFIER+')\s*\)\s*$', line):
                 return
 
             # parse error
             raise ScriptParseError("Unable to parse line %d: %s" % (lineno,line) )
 
             lineno += 1
-
 

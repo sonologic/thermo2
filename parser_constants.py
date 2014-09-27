8f8f8ddde5a03c1c97af71839e8277b97fc546f5
Koen Martens <gmc@sonologic.nl>
2014-09-27 17:08:36 +0200

    Add negative integer and float to ParserConstants.

--- parser_constants.py
+++ parser_constants.py
@@ -1,6 +1,7 @@
 class ParserConstants:
     RE_IDENTIFIER   = '[A-Za-z_][A-Za-z0-9_]*'
-    RE_INTEGER      = '[0-9]+'
+    RE_INTEGER      = '-?[0-9]+'
+    RE_FLOAT        = '-?[0-9]+(\.[0-9]+)?'
     RE_MATH_OP      = '[+-/*]'
     RE_TERM         = '('+RE_IDENTIFIER+')|('+RE_INTEGER+')'
     RE_COMPARE      = '<|>|==|!=|>=|<='

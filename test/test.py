15b4705016a8e71e82a93d08cec507f25276e8ed
Koen Martens <gmc@sonologic.nl>
2016-03-05 22:30:50 +0100

    test.py: add logger, add testcases

--- test/test.py
+++ test/test.py
@@ -2,9 +2,11 @@
 
 import unittest
 import sys, os
+import logging
 
 sys.path.insert(0, "../")
 
+from loghandler import LogHandler
 from test_thermo2 import Thermo2Test
 from test_scheduler import SchedulerTest
 from test_timer import TimerTest
@@ -12,6 +14,7 @@ from test_event import EventTest
 from test_event_listener import EventListenerTest
 from test_sensor_event import SensorEventTest
 from test_sensor import SensorTest
+from test_sensor_json import JsonSensorTest
 from test_event_processor import EventProcessorTest
 from test_event_processor_delay import EventProcessorDelayTest
 from test_script import ScriptTest
@@ -23,13 +26,14 @@ from test_config import ConfigTest
 
 if __name__ == "__main__":
 
+        logger = logging.getLogger('')
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(LogHandler())
+
         suite = []
 
         suite.append(unittest.TestLoader().loadTestsFromTestCase(MyexceptionsTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(LineReaderTest))
-        suite.append(unittest.TestLoader().loadTestsFromTestCase(Thermo2Test))
-        suite.append(unittest.TestLoader().loadTestsFromTestCase(SchedulerTest))
-        suite.append(unittest.TestLoader().loadTestsFromTestCase(ConfigTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(TimerTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(EventTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(EventListenerTest))
@@ -37,9 +41,15 @@ if __name__ == "__main__":
         suite.append(unittest.TestLoader().loadTestsFromTestCase(EventProcessorDelayTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(SensorEventTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(SensorTest))
+        suite.append(unittest.TestLoader().loadTestsFromTestCase(JsonSensorTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(ScriptTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(ScriptExpressionTest))
         suite.append(unittest.TestLoader().loadTestsFromTestCase(ProcessTest))
+        suite.append(unittest.TestLoader().loadTestsFromTestCase(SchedulerTest))
+        suite.append(unittest.TestLoader().loadTestsFromTestCase(ConfigTest))
+        suite.append(unittest.TestLoader().loadTestsFromTestCase(Thermo2Test))
+
+        suites
 
         unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(suite))
 

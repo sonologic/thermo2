9c2568674fee2eed4dcd97fb0529cad152855337
Koen Martens <gmc@sonologic.nl>
2016-03-05 16:54:12 +0100

    sensor_json.py: implement http get and fire event

--- sensor_json.py
+++ sensor_json.py
@@ -1,4 +1,23 @@
 from sensor import Sensor
+import requests
+import json
+from sensor_event import SensorEvent
+from time import time
 
 class JsonSensor(Sensor):
-    pass
+
+    def __init__(self,event,trigger):
+        Sensor.__init__(self,event,trigger)
+
+    def getValue(self):
+        try:
+            response = requests.get(self.url)
+        except requests.exceptions.ConnectionError as e:
+            print str(e)
+            return
+
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            if 'value' in data:
+                self.value = data['value']
+

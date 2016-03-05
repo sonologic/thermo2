563373213144c2abccf080e52a690a357b7122a5
Koen Martens <gmc@sonologic.nl>
2016-03-05 22:46:17 +0100

    main.py: refactor to use LogHandler from module

--- main.py
+++ main.py
@@ -5,15 +5,7 @@ from config import Config
 from thermo2 import Thermo2
 from time import sleep
 import logging
-
-class LogHandler(logging.Handler):
-    def __init__(self):
-        logging.Handler.__init__(self)
-        self.setFormatter(logging.Formatter(fmt="%(asctime)s %(levelname)s %(message)s"))
-
-    def emit(self, record):
-        print self.format(record)
-
+from loghandler import LogHanlder
 
 class Main(object):
     def __init__(self):

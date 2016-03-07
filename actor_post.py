f7c000c46af9ab1b88e2991bb8c5d74f0f321129
Koen Martens <gmc@sonologic.nl>
2016-03-07 17:22:08 +0100

    actor_post.py: log and continue on connection error

--- actor_post.py
+++ actor_post.py
@@ -1,12 +1,20 @@
 from actor import Actor
 import requests
+import logging
 
 class PostActor(Actor):
+    def __init__(self):
+        Actor.__init__(self)
+        self.logger = logging.getLogger('thermo2.'+__name__)
+
     def actOnValue(self, label, t, value):
         postData = {
             'label' : label,
             't'     : t,
             'value' : value,
         }
-        requests.post(self.url, data=postData)
+        try:
+            requests.post(self.url, data=postData)
+        except requests.exceptions.ConnectionError as e:
+            self.logger.error(e)
 

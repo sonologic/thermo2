31df49a5cd823a9dc76ebcfd595de819392228a8
Koen Martens <gmc@sonologic.nl>
2016-03-13 21:53:55 +0100

    cs Thermo2Cli: muck about with command line parsing

--- src/cs/Thermo2Cli/Thermo2Cli/Thermo2Cli.cs
+++ src/cs/Thermo2Cli/Thermo2Cli/Thermo2Cli.cs
@@ -13,8 +13,11 @@ namespace Thermo2Cli
         [Option('s', "set", HelpText = "Inject event")]
         public string Set { get; set; }
 
-        [Option('g', "get", HelpText = "Get cachd event")]
+        [Option('g', "get", HelpText = "Get cached event")]
         public string Get { get; set; }
+
+        [Option('v', "value", HelpText = "Value of event to inject")]
+        public string Value { get; set; }
 
         [HelpOption]
         public string GetUsage()
@@ -27,10 +30,53 @@ namespace Thermo2Cli
 
     class Thermo2Cli
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            
-            Console.WriteLine("Hello, world..");
+            var options = new Options();
+            CommandLine.Parser parser = new Parser();
+            if(parser.ParseArguments(args, options)) {
+                Console.WriteLine("Get: ");
+                if(options.Get!=null) {
+                  Console.WriteLine(options.Get);
+                } else {
+                  Console.WriteLine("null");
+                }
+
+                Console.WriteLine("Set: ");
+                if(options.Set!=null) {
+                  Console.WriteLine(options.Set);
+                } else {
+                  Console.WriteLine("null");
+                }
+
+                Console.WriteLine("Value: ");
+                if(options.Value!=null) {
+                  Console.WriteLine(options.Value);
+                } else {
+                  Console.WriteLine("null");
+                }
+
+                if(options.Get==null && options.Set==null) {
+                    Console.WriteLine("Either specify -s or -g");
+                    return -1;
+                }
+
+                var api = new Api();
+
+                if(options.Get!=null) {
+                    // get
+                    Value get_value = api.Get(options.Get);
+                    Console.WriteLine(get_value);
+                    return 0;
+                } else {
+                    // set
+                    Console.WriteLine("Set not implemented yet..");
+                    return -1;
+                }
+            } else {
+                Console.WriteLine(options.GetUsage());
+                return -1;
+            }
         }
     }
 }

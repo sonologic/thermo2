e60868cdbaa22d22ddc7db300a1a8f0fe456b888
Koen Martens <gmc@sonologic.nl>
2016-03-13 02:13:01 +0100

    Initial commit of c# Thermo2Cli

---
+++ src/cs/Thermo2Cli/Thermo2Cli/Thermo2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Thermo2
+{
+    enum ValueTypes { IntValue, StringValue, FloatValue };
+
+    public class Value
+    {
+        ValueTypes Type;
+        int int_value;
+        float float_value;
+        string string_value;
+
+        public Value(int value)
+        {
+            Type = ValueTypes.IntValue;
+            int_value = value;
+        }
+
+        public Value(float value)
+        {
+            Type = ValueTypes.FloatValue;
+            float_value = value;
+        }
+
+        public Value(string value)
+        {
+            Type = ValueTypes.StringValue;
+            string_value = value;
+        }
+    }
+
+    public class Api
+    {
+        private string Host;
+        private int Port;
+
+        public Api(string Host, int Port)
+        {
+            this.Host = Host;
+            this.Port = Port;
+        }
+
+        public Api()
+        {
+            Host = "127.0.0.1";
+            Port = 8822;
+        }
+
+        public Value Get(string label)
+        {
+            return null;
+        }
+
+        public void Set(string label, float value, float t)
+        {
+
+        }
+    }
+}
---
+++ src/cs/Thermo2Cli/Thermo2Cli/Thermo2Cli.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thermo2;
+using CommandLine;
+
+namespace Thermo2Cli
+{
+    class Options
+    {
+        [Option('s', "set", HelpText = "Inject event")]
+        public string Set { get; set; }
+
+        [Option('g', "get", HelpText = "Get cachd event")]
+        public string Get { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var usage = new StringBuilder();
+            usage.AppendLine("Thermo2 client");
+            return usage.ToString();
+        }
+    }
+
+    class Thermo2Cli
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, world..");
+        }
+    }
+}

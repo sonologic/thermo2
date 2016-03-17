f9d16fda581d6760358b66e8a6219f8988ea4cab
Koen Martens <gmc@sonologic.nl>
2016-03-17 20:19:25 +0100

    main.cc: catch exceptions, print result

--- src/cc/main.cc
+++ src/cc/main.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 #include "Api.h"
 
 using namespace std;
@@ -7,7 +8,23 @@ int main()
 {
     Thermo2Api api = Thermo2Api();
 
-    Thermo2Event *event = api.Get("sum_value");
+    Thermo2Event *event = nullptr;
+
+    try {
+        event = api.Get("sum_value");
+    } catch(const char *e) {
+        cout << "error: " << e << endl;
+        return -1;
+    } catch(...) {
+        cout << "default exception" << endl;
+        return -1;
+    }
+
+    cout.precision(7);
+
+    cout << event->label << ":" << event->value << ":" << event->t << endl;
+
+    delete event;
     
     return 0;
 }

a502f40044d5f1371352321a39300d10c195fc74
Koen Martens <gmc@sonologic.nl>
2016-03-25 14:15:45 +0100

    cs: run tests with mono --debug

--- src/cs/Thermo2Cli/test.sh
+++ src/cs/Thermo2Cli/test.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-mono packages/xunit.runner.console.2.1.0/tools/xunit.console.exe Thermo2Cli.Tests/bin/Debug/Thermo2Cli.Tests.dll $@
+mono --debug packages/xunit.runner.console.2.1.0/tools/xunit.console.exe Thermo2Cli.Tests/bin/Debug/Thermo2Cli.Tests.dll $@
